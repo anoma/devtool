c3e15de1a325c44b03f97bc361fd7baa72475a2b
James Hiew <james@chainfork.com>
2022-09-28 12:39:24 +0100

    Fix up to work with experimental Eth bridge branch

--- src/tendermint.rs
+++ src/tendermint.rs
@@ -1,24 +1,22 @@
 use namada::proto::Tx;
 use namada::tendermint::node::info::ListenAddress;
-use namada_apps::client::tendermint_websocket_client::{
-    TendermintWebsocketClient, WebSocketAddress,
-};
+use namada_apps::facade::tendermint_rpc::{Client, WebSocketClient};
 use namada_apps::tendermint_config::net::Address as TendermintAddress;
-use namada_apps::tendermint_rpc::Client;
-use std::time::Duration;
 
 const RAW_RPC_ADDR: &str = "tcp://127.0.0.1:26657";
 
-fn get_tm_websocket_client(rpc_addr: &str) -> eyre::Result<TendermintWebsocketClient> {
+async fn get_tm_websocket_client(rpc_addr: &str) -> eyre::Result<WebSocketClient> {
     let listen_addr = ListenAddress::new(rpc_addr.to_string());
     let remote_addr = TendermintAddress::from_listen_address(&listen_addr).unwrap();
-    let websocket_addr = WebSocketAddress::try_from(remote_addr)?;
-    let client = TendermintWebsocketClient::open(websocket_addr, Some(Duration::from_secs(5)))?;
+    let (client, driver) = WebSocketClient::new(remote_addr).await?;
+    tokio::spawn(async move {
+        driver.run().await.unwrap();
+    });
     Ok(client)
 }
 
 pub async fn submit(tx: Tx) -> eyre::Result<()> {
-    let client = get_tm_websocket_client(RAW_RPC_ADDR)?;
+    let client = get_tm_websocket_client(RAW_RPC_ADDR).await?;
     let tm_status = client.status().await.unwrap();
     tracing::info!(?tm_status, "Got status from Tendermint");
     let resp = client.broadcast_tx_commit(tx.to_bytes().into()).await?;
